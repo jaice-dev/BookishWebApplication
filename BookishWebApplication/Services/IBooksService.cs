173a420e67f91be67754c5acd5a72f498cbeb15c
Jordan <jordan.d.carlton@gmail.com>
2021-11-03 17:27:57 +0000

    Refactored Database request code

--- BookishWebApplication/Services/IBooksService.cs
+++ BookishWebApplication/Services/IBooksService.cs
@@ -20,19 +20,37 @@ namespace BookishWebApplication.Services
 
         public IEnumerable<Book> GetAllBooks()
         {
+            var getBooksQuery =
+                "SELECT book.id as bookId, title, publicationyear, isbn, author.id as authorId, firstname, lastname FROM bookauthor, book, author WHERE bookauthor.bookid = book.id AND bookauthor.authorid = author.id";
+            
+            return GetDatabaseResponse(getBooksQuery, null);
+        }
+        
+        public IEnumerable<Book> SearchBooks(string searchString)
+        {
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var searchParameters = new DynamicParameters(new {SearchTitle = "%" + searchString + "%"});
+
+                var searchBooksQuery =
+                    "SELECT book.id as bookId, title, publicationyear, isbn, author.id as authorId, firstname, lastname FROM bookauthor, book, author WHERE bookauthor.bookid = book.id AND bookauthor.authorid = author.id AND lower(title) LIKE lower(@SearchTitle)";
+
+                return GetDatabaseResponse(searchBooksQuery, searchParameters);
+            }
+
+            return null;
+        }
+
+        private static IEnumerable<Book> GetDatabaseResponse(string searchSql, DynamicParameters parameters = null)
+        {
             using var connection = new NpgsqlConnection(connectionString);
-
-            var countPrintsQuery = "SELECT bookId, COUNT(id) FROM print GROUP BY bookId";
-
-            var countsByBookId = connection.Query(countPrintsQuery).ToDictionary(row => (int) row.bookid, row => (int) row.count);
+            
+            var countsByBookId = GetPrintCountDict(connection);
 
             var bookDictionary = new Dictionary<int, Book>();
 
-            var getBooksQuery =
-                "SELECT book.id as bookId, title, publicationyear, isbn, author.id as authorId, firstname, lastname FROM bookauthor, book, author WHERE bookauthor.bookid = book.id AND bookauthor.authorid = author.id";
-
             return connection.Query<Book, Author, Book>(
-                    getBooksQuery,
+                    searchSql,
                     (book, author) =>
                     {
                         Book currentBook;
@@ -41,54 +59,30 @@ namespace BookishWebApplication.Services
                         {
                             currentBook = book;
                             currentBook.Authors = new List<Author>();
-                            currentBook.PrintCount = countsByBookId.ContainsKey(currentBook.BookId) ? countsByBookId[currentBook.BookId] : 0;
+                            currentBook.PrintCount = countsByBookId.ContainsKey(currentBook.BookId)
+                                ? countsByBookId[currentBook.BookId]
+                                : 0;
                             bookDictionary.Add(currentBook.BookId, currentBook);
                         }
-                        
+
                         currentBook.Authors.Add(author);
 
                         return currentBook;
                     },
-                    splitOn: "authorId"
+                    splitOn: "authorId",
+                    param: parameters
                 )
                 .Distinct();
         }
-        
 
-        public IEnumerable<Book> SearchBooks(string searchString)
+        private static Dictionary<int, int> GetPrintCountDict(NpgsqlConnection connection)
         {
-            if (!string.IsNullOrEmpty(searchString))
-            {
-                var parameters = new DynamicParameters( new { SearchTitle = "%" + searchString + "%" } );
-                var bookDictionary = new Dictionary<int, Book>();
+            var countPrintsQuery = "SELECT bookId, COUNT(id) FROM print GROUP BY bookId";
 
-                var sql = "SELECT book.id as bookId, title, publicationyear, isbn, author.id as authorId, firstname, lastname FROM bookauthor, book, author WHERE bookauthor.bookid = book.id AND bookauthor.authorid = author.id AND lower(title) LIKE lower(@SearchTitle)";
-
-                using var connection = new NpgsqlConnection(connectionString);
-                return connection.Query<Book, Author, Book>(
-                        sql,
-                        (book, author) =>
-                        {
-                            Book currentBook;
-
-                            if (!bookDictionary.TryGetValue(book.BookId, out currentBook))
-                            {
-                                currentBook = book;
-                                currentBook.Authors = new List<Author>();
-                                bookDictionary.Add(currentBook.BookId, currentBook);
-                            }
-                        
-                            currentBook.Authors.Add(author);
-
-                            return currentBook;
-                        },
-                        splitOn: "authorId",
-                        param: parameters
-                    )
-                    .Distinct();
-            }
-
-            return new List<Book>();
+            var countsByBookId =
+                connection.Query(countPrintsQuery).ToDictionary(row => (int) row.bookid, row => (int) row.count);
+            return countsByBookId;
         }
+        
     }
 }
