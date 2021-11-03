22bae2ab6fbb87bedbe369e07e8e9388f9f6f77b
Temuujin Natsagnyam <temuujin.natsagnyam@softwire.com>
2021-11-03 15:59:40 +0000

    searchbar fix

--- BookishWebApplication/Services/IBooksService.cs
+++ BookishWebApplication/Services/IBooksService.cs
@@ -54,12 +54,32 @@ namespace BookishWebApplication.Services
             if (!string.IsNullOrEmpty(searchString))
             {
                 var parameters = new DynamicParameters( new { SearchTitle = "%" + searchString + "%" } );
-                var sql = @"SELECT * FROM bookauthor
-                            JOIN book on bookauthor.bookid = book.Id
-                            JOIN author on bookauthor.authorid = author.id
-                            WHERE lower(title) LIKE lower(@SearchTitle)";
+                var bookDictionary = new Dictionary<int, Book>();
+
+                var sql = "SELECT book.id as bookId, title, publicationyear, isbn, author.id as authorId, firstname, lastname FROM bookauthor, book, author WHERE bookauthor.bookid = book.id AND bookauthor.authorid = author.id AND lower(title) LIKE lower(@SearchTitle)";
+
                 using var connection = new NpgsqlConnection(connectionString);
-                return connection.Query<Book>(sql, parameters);
+                return connection.Query<Book, Author, Book>(
+                        sql,
+                        (book, author) =>
+                        {
+                            Book currentBook;
+
+                            if (!bookDictionary.TryGetValue(book.BookId, out currentBook))
+                            {
+                                currentBook = book;
+                                currentBook.Authors = new List<Author>();
+                                bookDictionary.Add(currentBook.BookId, currentBook);
+                            }
+                        
+                            currentBook.Authors.Add(author);
+
+                            return currentBook;
+                        },
+                        splitOn: "authorId",
+                        param: parameters
+                    )
+                    .Distinct();
             }
 
             return new List<Book>();
