4aa169e2d009d74aab55bafafad9c066966fc434
Jordan <jordan.d.carlton@gmail.com>
2021-11-04 19:59:37 +0000

    add stuff

---
+++ BookishWebApplication/Models/View/Author/AuthorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BookishWebApplication.Models.Database;
+
+namespace BookishWebApplication.Models.View
+{
+    public class AuthorViewModel
+    {
+        public Author Author { get; set; }
+        public IEnumerable<Book> BooksByAuthor { get; set; }
+    }
+}
